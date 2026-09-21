bd67ce9dd8410d939bc4cb024888499d37e4e348
viveav <morozovavioletta1808@gmail.com>
2026-09-21 19:41:54 +0500

    Выполнила задания 3

--- src/task3-assertion.ts
+++ src/task3-assertion.ts
@@ -1,19 +1,29 @@
 // Тип конфигурации приложения
 export type AppConfig = {
-  apiUrl: string;
-  timeout: number;
+    apiUrl: string;
+    timeout: number;
 };
 
 // Напишите Assertion Function assertValidConfig
 // Если данные не соответствуют AppConfig - бросить Error("Invalid config")
 // Используйте синтаксис: asserts data is AppConfig
 export function assertValidConfig(data: unknown): asserts data is AppConfig {
-  // Напишите код здесь
+    if (
+        typeof data !== "object" ||
+        data === null ||
+        !("apiUrl" in data) ||
+        !("timeout" in data) ||
+        typeof (data as any).apiUrl !== "string" ||
+        typeof (data as any).timeout !== "number"
+    ) {
+        throw new Error("Invalid config");
+    }
 }
 
 // Функция инициализации.
 // Сначала вызывает assertValidConfig(config).
 // Если проверка прошла - вернуть "API: <apiUrl>, Timeout: <timeout>"
 export function initApp(config: unknown): string {
-  // Напишите код здесь
+    assertValidConfig(config);
+    return `API: ${config.apiUrl}, Timeout: ${config.timeout}`;
 }
